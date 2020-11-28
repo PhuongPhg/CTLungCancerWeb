a62954c729e514e88bdaa922b0c41e0f61a8fd61
Athos <dracarys1312@gmail.com>
2020-11-28 21:09:23 +0700

    Add Yandex as source

---
+++ crawler/one/one/spiders/YandexSpyder.py
@@ -0,0 +1,18 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from bs4 import BeautifulSoup
+import urllib
+class YandexSpider(scrapy.spiders.Spider):
+    name = 'crawlImages'
+    start_urls = ['https://yandex.com/images/search?text=lung%20cancer%20ct%20scan']
+    def parse(self, response):
+        titles = response.css('img::attr(alt)').extract()
+        links = response.css('img::attr(src)').extract()
+        print('##########')
+        for item in zip(titles, links):
+            all_items = {
+                'Title' : BeautifulSoup(item[0]).text,
+                'Link' :  "https:" + item[1]
+            }
+            print(item[1])
+            yield all_items
